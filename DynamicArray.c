7849e29f3a798884cfe58dbbdbf2feb76ab50fd7
Md Abdulla Al Mamun <mamun.cseian@gmail.com>
2019-05-19 14:12:44 +0600

    Some changes for easy understanding Dynamic Array

--- DynamicArray.c
+++ DynamicArray.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//running time you decided your array size
+void DynamicArray(int *A, int size)
+{
+    A = (int *)malloc(size * sizeof(int));
+}
+void ReadArray(int *A, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        scanf("%d",&A[i]);
+}
+void DisplayArray(int *A, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ",&A[i]);
+    printf("\n");
+}
+
 int main()
 {
     int n;
@@ -9,21 +28,8 @@ int main()
     printf("Enter the size of Array :");
     scanf("%d", &n);
     size = n;
-    //running time you decided your array size
-    A = (int *)malloc(n*sizeof(int));
-    printf("Do you want more size in Array, Enter it: ");
-    scanf("%d",&n);
-    size += n;
-    //running time you decided your array size
-    A = (int *)malloc(n*sizeof(int));
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d",&A[i]);
-    }
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
+    DynamicArray(A, n);
+    ReadArray(A, size);
+    DisplayArray(A, size);
 
 }
