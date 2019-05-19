77f7d1a9f3151227dc196a8863eaf91d1265b204
Md Abdulla Al Mamun <mamun.cseian@gmail.com>
2019-05-19 14:31:59 +0600

    Some error has gone

--- DynamicArray.c
+++ DynamicArray.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//running time you decided your array size
-void DynamicArray(int *A, int size)
-{
-    A = (int *)malloc(size * sizeof(int));
-}
 void ReadArray(int *A, int size)
 {
     int i;
@@ -16,7 +11,7 @@ void DisplayArray(int *A, int size)
 {
     int i;
     for (i = 0; i < size; i++)
-        printf("%d ",&A[i]);
+        printf("%d ",A[i]);
     printf("\n");
 }
 
@@ -28,7 +23,8 @@ int main()
     printf("Enter the size of Array :");
     scanf("%d", &n);
     size = n;
-    DynamicArray(A, n);
+//running time you decided your array size
+    A = (int *)malloc(size * sizeof(int));
     ReadArray(A, size);
     DisplayArray(A, size);
 
